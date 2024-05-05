158a9351908233e4c2e191512802b46f050a3a02
Anderson Menezes <anderson.stifip@gmail.com>
2024-05-05 02:42:04 -0300

    feat: ajustes visuais

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Toaster } from "sonner";
 import { Wrapper } from "./components/app/wrapper";
 
 export function App() {
   return (
     <>
       <Wrapper />
+      <Toaster richColors position="bottom-center" />
     </>
   );
 }
--- src/components/app/wrapper.tsx
+++ src/components/app/wrapper.tsx
@@ -1,8 +1,9 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { cnpj, cpf } from "cpf-cnpj-validator";
-import { Copy, FileCheck2Icon } from "lucide-react";
+import { Copy, FileCheck2Icon, RotateCcw } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { z } from "zod";
 import { Button } from "../ui/button";
 import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
@@ -35,10 +36,12 @@ export function Wrapper() {
     switch (type) {
       case "cpf":
         setDocument(cpf.generate());
+        toast.success("CPF gerado com sucesso!");
         return document;
         break;
       case "cnpj":
         setDocument(cnpj.generate());
+        toast.success("CNPJ gerado com sucesso!");
         return document;
         break;
     }
@@ -63,13 +66,24 @@ export function Wrapper() {
   }
 
   function handleClipBoardCopy() {
+    const type = form.getValues("type");
+
     navigator.clipboard.writeText(document);
+    toast.info(type === "cpf" ? `CPF copiado` : `CNPJ copiado`);
+  }
+
+  function handleResetForm() {
+    form.reset();
+    setDocument("");
+    setDocumentFormatted("");
+    setIsFormated(false);
+    toast.info("Documento resetado!");
   }
 
   useEffect(handleIsFormated, [isFormated, document, form]);
 
   return (
-    <div className="flex flex-col items-center m-auto text-foreground h-screen w-[400px] gap-3 p-8 pb-4">
+    <div className="flex flex-col items-center m-auto text-foreground h-screen min-h-[500px] w-[400px] gap-3 p-8 pb-4">
       <header className="mt-4 text-3xl font-bold tracking-tight w-full text-center">
         <div className="flex justify-center items-center gap-2 pb-4">
           <FileCheck2Icon className="text-emerald-500" size={30} />
@@ -124,7 +138,11 @@ export function Wrapper() {
                 Formatado?
               </Label>
             </div>
-            <Button className="bg-emerald-500" type="submit">
+            <Button
+              className="bg-emerald-500"
+              type="submit"
+              disabled={form.formState.isSubmitting}
+            >
               Gerar
             </Button>
           </div>
@@ -133,15 +151,19 @@ export function Wrapper() {
             <Input
               id="document"
               type="text"
-              placeholder="Documento gerado"
+              placeholder={
+                document ? "Documento gerado" : "Nenhum documento gerado"
+              }
               readOnly
               maxLength={14}
+              className="py-6"
               value={isFormated ? documentFormatted : document}
             />
             <Button
               variant="ghost"
               type="button"
-              className="absolute right-0 top-0 hover:bg-emerald-500 group"
+              className="absolute right-0 top-0 hover:bg-emerald-500 group h-full"
+              disabled={!document}
               onClick={handleClipBoardCopy}
             >
               <Copy
@@ -149,11 +171,23 @@ export function Wrapper() {
                 className="text-gray-400 group-hover:bg-emerald-500 group-hover:text-gray-100"
               />
             </Button>
+            <Button
+              variant="ghost"
+              type="button"
+              className="absolute right-12 top-0 group h-full"
+              disabled={!document}
+              onClick={handleResetForm}
+            >
+              <RotateCcw
+                size={16}
+                className="text-gray-400 group-hover:text-rose-400 bg-transparent"
+              />
+            </Button>
           </div>
         </form>
       </Form>
 
-      <footer className="text-sm text-muted-foreground pt-6 pb-6">
+      <footer className="text-sm text-muted-foreground pt-6 pb-4 bottom-0 fixed">
         Produzido por &copy; Anderson Menezes - {new Date().getFullYear()}
       </footer>
     </div>
